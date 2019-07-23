f7be95899c26d1f14b6bedaddf6a3f3454e41d5f
Darnell Harris <d.michael.harri92@gmail.com>
2019-07-23 17:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+   let array = [];
+  
+  for(var i = 0; i < musicians.length; i++){
+    array.push(`musicians[i] plays instruments[i]`);
+  }
+  return array;
+}
+
+function johnLennonFacts(){
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+var newFact =[];
+var i = 0;
+
+  while(i < facts.length){
+    newFact.push(facts[i] + "!!!");
+    i++
+  }
+  return newFact;
+}
+
+function iLoveTheBeatles(num){
+  var arr = [];
+  
+  do{
+    arr.push("I love the Beatles!");
+  }
+  while(num < 15){
+    num++;
+  }
+  
+  return arr;
+}
